75d215bc7091f0512228cbc7f6583709c2098d8e
OksanaCTD <fetter.oksana@gmail.com>
2024-07-30 00:47:11 -0400

    Build and test a simple Rails application using a scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
+  delete '/users/logoff', to: 'users#logoff', as: 'logoff_user'
+  post '/users/:id/logon', to: 'users#logon', as: 'logon_user'
+
+  # resources :users
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  post '/users', to: 'users#create'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+
+  resources :users
+  resources :forums
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
